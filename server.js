47b9ccac7184df18a316d329b76297577fc6abd1
kazukinakamuraKN <kajirikajiri@gmail.com>
2021-08-20 21:30:19 +0900

    Passing Arguments
    https://graphql.org/graphql-js/passing-arguments/

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
-var express = require('express')
-var {buildSchema} = require('graphql')
-var {graphqlHTTP}=require('express-graphql')
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
 
+// Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
-  type Query{
-    hello: String
+  type Query {
+    rollDice(numDice: Int!, numSides: Int): [Int]
   }
-`)
+`);
 
-var root={
-  hello:()=>{
-    return `hello world!`
+// The root provides a resolver function for each API endpoint
+var root = {
+  rollDice: ({numDice, numSides}) => {
+    var output = [];
+    for (var i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    }
+    return output;
   }
-}
+};
 
-var app=express()
-app.use('/graphql',graphqlHTTP({
-  schema,
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
   rootValue: root,
-  graphiql: true
-}))
-app.listen(4000)
-console.log('running')
+  graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
